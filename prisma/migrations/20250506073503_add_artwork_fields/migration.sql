dfc98477330cecdf00640bc2afd64d02b73c3ba1
Mchumbles <lewis.mcguire92@yahoo.com>
2025-05-06 08:39:12 +0100

    Migrate prisma schema for exhibition + artwork tables

--- prisma/migrations/20250506073503_add_artwork_fields/migration.sql
+++ prisma/migrations/20250506073503_add_artwork_fields/migration.sql
@@ -13,7 +13,7 @@ CREATE TABLE "users" (
 CREATE TABLE "exhibitions" (
     "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
-    "location" TEXT NOT NULL,
+    "location" TEXT,
     "category" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "userId" TEXT NOT NULL,
@@ -26,10 +26,17 @@ CREATE TABLE "artworks" (
     "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "artist" TEXT NOT NULL,
-    "date" TIMESTAMP(3) NOT NULL,
-    "image" TEXT NOT NULL,
-    "url" TEXT NOT NULL,
+    "date" TEXT,
+    "image" TEXT,
+    "url" TEXT,
+    "source" TEXT,
+    "description" TEXT,
+    "medium" TEXT,
+    "culture" TEXT,
+    "creditLine" TEXT,
+    "objectWikidata_URL" TEXT,
     "exhibitionId" TEXT NOT NULL,
+    "userId" TEXT,
 
     CONSTRAINT "artworks_pkey" PRIMARY KEY ("id")
 );
@@ -38,7 +45,10 @@ CREATE TABLE "artworks" (
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- AddForeignKey
-ALTER TABLE "exhibitions" ADD CONSTRAINT "exhibitions_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "exhibitions" ADD CONSTRAINT "exhibitions_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "artworks" ADD CONSTRAINT "artworks_exhibitionId_fkey" FOREIGN KEY ("exhibitionId") REFERENCES "exhibitions"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "artworks" ADD CONSTRAINT "artworks_exhibitionId_fkey" FOREIGN KEY ("exhibitionId") REFERENCES "exhibitions"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "artworks" ADD CONSTRAINT "artworks_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
